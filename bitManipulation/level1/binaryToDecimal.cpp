e13ac72494b7b98531d60704253881b8cadeed30
Arpt-Singh <arpit181001002080@gmail.com>
2024-11-30 00:48:31 +0530

    add binary to decimal

---
+++ bitManipulation/level1/binaryToDecimal.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binaryToDecimal(string binary){
+    int len = binary.size(), p2 = 1,num = 0;
+    for(int i=len-1;i>=0;i--){
+        if(binary[i]=='1'){
+            num = num + p2;
+        }
+        p2 = p2*2;
+    }
+    return num;
+}
+
+int main(){
+    string s ="11110";
+    cout<<binaryToDecimal(s);
+    return 0;
+}
